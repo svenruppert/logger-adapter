df4da258b62284c9c28d535a1a07a17fb0315df0
Sven Ruppert <sven.ruppert@gmail.com>
2019-09-05 10:12:28 +0200

    versio updates , prepare for 01.00.04-RPM

--- src/main/java/org/rapidpm/dependencies/core/logger/HasLogger.java
+++ src/main/java/org/rapidpm/dependencies/core/logger/HasLogger.java
@@ -15,10 +15,8 @@
  */
 package org.rapidpm.dependencies.core.logger;
 
-import org.rapidpm.dependencies.core.logger.factory.StandardLoggerFactory;
-
 public interface HasLogger {
-  public default LoggingService logger(){
+  default LoggingService logger(){
     return Logger.getLogger(getClass());
   }
 
